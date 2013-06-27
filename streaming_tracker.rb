e00bec30f7c374d05471e69350c0e2961370656c
José Carlos García <jcarlosgarcia@hotmail.com>
2013-06-27 21:29:43 +0200

    OAuth added

--- streaming_tracker.rb
+++ streaming_tracker.rb
@@ -1,28 +1,37 @@
-require 'eventmachine'
 require 'em-http'
+require 'em-http/middleware/oauth'
 require 'json'
 
-usage = "#{$0} <user> <password> <track>"
-abort usage unless user = ARGV.shift
-abort usage unless password = ARGV.shift
+usage = "#{$0} <track>"
+
 abort usage unless keywords = ARGV.shift
 
-def track(user, password, keywords)
+OAuthConfig = {
+  :consumer_key => '<your consumer key>',
+  :consumer_secret => '<your consumer secret>',
+  :access_token => '<your access token>',
+  :access_token_secret => '<your access token secret>'
+}
+
+def track(keywords)
   EventMachine.run do
-    http = EventMachine::HttpRequest.new("https://stream.twitter.com/1.1/statuses/filter.json",
+    conn = EventMachine::HttpRequest.new("https://stream.twitter.com/1.1/statuses/filter.json",
       :connection_timeout => 0,
-      :inactivity_timeout => 0).post(
-      :head => {'Authorization' => [user, password]}, 
+      :inactivity_timeout => 0)
+    
+    conn.use EventMachine::Middleware::OAuth, OAuthConfig
+
+    http = conn.post(
       :body => {'track' => keywords},
       :keepalive => true,
-      :timeout => -1)
+      :timeout => -1) 
 
       buffer = ""
       http.stream do |chunk|
         buffer += chunk
         while line = buffer.slice!(/.+\r?\n/)
           tweet = JSON.parse(line)
-          puts Time.new.to_s+"#{tweet['user']['screen_name']}: #{tweet['text']}"
+          puts Time.new.to_s + "#{tweet['user']['screen_name']}: #{tweet['text']}"
         end
 
       end
@@ -37,7 +46,4 @@ def track(user, password, keywords)
     puts "Error " + ex.to_s
 end
 
-while true
-    track user, password, keywords
-end
-
+track keywords
